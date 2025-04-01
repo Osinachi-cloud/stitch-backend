e452630ad26b6c8edb1f4f708a5bb4493df93cb0
chukwudozie <emkychuks062@gmail.com>
2025-04-01 15:05:33 +0100

    Payment APIs refactoring

--- payment/src/main/java/com/stitch/payment/exception/PaymentException.java
+++ payment/src/main/java/com/stitch/payment/exception/PaymentException.java
@@ -4,22 +4,14 @@ import com.stitch.commons.exception.StitchException;
 
 public class PaymentException extends StitchException {
 
-    public PaymentException() {
-    }
+    final int code;
 
-    public PaymentException(String message) {
+    public PaymentException(String message, int code) {
         super(message);
+        this.code = code;
     }
 
-    public PaymentException(String message, Throwable cause) {
-        super(message, cause);
-    }
-
-    public PaymentException(Throwable cause) {
-        super(cause);
-    }
-
-    public PaymentException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
-        super(message, cause, enableSuppression, writableStackTrace);
+    public int getCode() {
+        return code;
     }
 }
