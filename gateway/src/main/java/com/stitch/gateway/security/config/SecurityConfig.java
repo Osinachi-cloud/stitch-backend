25274f3b639ee8928f95ed6a96ec3e44615ac7e8
chukwudozie <emkychuks062@gmail.com>
2025-07-19 15:18:35 +0100

    code clean up

--- gateway/src/main/java/com/stitch/gateway/security/config/SecurityConfig.java
+++ gateway/src/main/java/com/stitch/gateway/security/config/SecurityConfig.java
@@ -26,9 +26,6 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static com.stitch.gateway.util.Constants.ALLOWED_URLS;
 
 @Configuration
